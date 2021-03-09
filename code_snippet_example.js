c230c07085f5b4fbb2fde711c6c4f410d68d8cc9
Douglas Mason <douglasmason@gmail.com>
2021-03-09 11:26:47 -0800

    Update code_snippet_example.js

--- code_snippet_example.js
+++ code_snippet_example.js
@@ -1,82 +1,67 @@
 
 //
-// The core code needed to reconstruct this demonstration is between this comment...
+// The core code needed to perform the business logic is between this comment...
 //
 
-import banditoAPI from '../bandito.js'
+import {headlineOptimizer} from '../bandito.js'
 
+var api_key_for_bandito = 'cILEqAj8kK9qphdlGRTmZ3LLwBs0s0mE9vgDFz2z'
+var headlines_to_consider = [
+    '<text style="color: blue;">Take the blue pill</text>',
+    '<text style="color: red;">Take the red pill</text>',
+]
+
+var bandit = new headlineOptimizer(api_key_for_bandito, 'app_id=code_snippet_example', headlines_to_consider)
+
+var most_recently_selected_headline = null
 var map_pill_color_to_reward = {
     'red': 0,
     'blue': 1
 }
 
-var most_recently_selected_feature_vector = null
-var api_key_for_bandito = 'cILEqAj8kK9qphdlGRTmZ3LLwBs0s0mE9vgDFz2z'
-var headlines_to_consider = {
-    'blue': '<text style="color: blue;">Take the blue pill</text>',
-    'red': '<text style="color: red;">Take the red pill</text>',
-}
-var feature_metadata = [{
-    'name': 'text_to_choose',
-    'categorical_or_continuous': 'categorical',
-    'possible_values': ['blue', 'red']
-}]
-var bandit_metadata = {
-    'model_id': 'app_id=code_snippet_example',
-    'model_type': 'AverageCategoryMembership', // options include CovarianceLinearRegression, LinearAlgebraLinearRegression, AverageCategoryMembership
-    'feature_vectors': [['blue'], ['red']],
-    'feature_metadata': feature_metadata
-}
-var bandit = new banditoAPI(
-    api_key_for_bandito,
-    bandit_metadata.model_id,
-    bandit_metadata.feature_metadata,
-    bandit_metadata.model_type
-)
-
 async function getHeadline() {
     updateHeadline('<h2>Thinking...</h2>')
-    var action_index = await bandit.select_with_automatic_restart(
-        bandit_metadata.feature_vectors,
-        bandit_metadata.model_type
-    )
-    updateHeadline(`<h2>${headlines_to_consider[bandit_metadata.feature_vectors[action_index][0]]}</h2>`)
     
-    var content = document.getElementById("training_progress")
-    content.innerHTML = `${(bandit.progress*100.0)/1.0.toFixed(2)}%`
+    var stopwatch_start = (new Date()).getTime()
+    var selected_headline = await bandit.selectHeadline()
+    var stopwatch_stop = (new Date()).getTime()
     
-    return bandit_metadata.feature_vectors[action_index]
+    updateLatency(`${stopwatch_stop - stopwatch_start} ms`)
+    updateHeadline(`<h2>${selected_headline}</h2>`)
+    updateProgress(`${(bandit.progress*100.0)/1.0.toFixed(2)}%`)    
+    return selected_headline
 }
 
 async function bluePill() {
     updateHeadline('<h2>Thinking...</h2>')
     var response = await bandit.train(
-        [most_recently_selected_feature_vector],
+        [most_recently_selected_headline],
         [map_pill_color_to_reward['blue']]
     )
-    most_recently_selected_feature_vector = await getHeadline()
+    most_recently_selected_headline = await getHeadline()
 }
 
 async function redPill() {
     updateHeadline('<h2>Thinking...</h2>')
     var response = await bandit.train(
-        [most_recently_selected_feature_vector],
+        [most_recently_selected_headline],
         [map_pill_color_to_reward['red']]
     )
-    most_recently_selected_feature_vector = await getHeadline()
+    most_recently_selected_headline = await getHeadline()
 }
 
 async function restart() {
     updateHeadline('<h2>Restarting...</h2>')
     updateProgress('0%')
     var response = await bandit.restart()
-    most_recently_selected_feature_vector = await getHeadline()
+    most_recently_selected_headline = await getHeadline()
 }
 
 
 //
-// ...and this comment
+// ...and this comment. The rest handles the visuals.
 //
+
 
 function updateHeadline(new_value) {
     var content = document.getElementById("headline")
@@ -88,8 +73,13 @@ function updateProgress(new_value) {
     content.innerHTML = new_value
 }
 
+function updateLatency(new_value) {
+    var content = document.getElementById("latency")
+    content.innerHTML = new_value
+}
+
 async function pull() {
-    most_recently_selected_feature_vector = await getHeadline()
+    most_recently_selected_headline = await getHeadline()
 }
 
 function updateOptimizePill() {
@@ -117,7 +107,7 @@ function changePillHasTheReward(pill_color) {
     
 }
 
-// Do first run automatically when the page loads
+// Do first pull and display update automatically when the page loads
 pull()
 updateOptimizePill()
 
@@ -128,7 +118,6 @@ document.getElementById("optimizeRedPill").addEventListener("click", function ()
 document.getElementById("optimizeBluePill").addEventListener("click", function () {
     changePillHasTheReward('blue')
 });
-
 document.getElementById("redPill").addEventListener("click", function () {
     redPill()
 });
